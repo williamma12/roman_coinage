2f328a377449023e1f3d7b3c8abd2c1b361e788c
William Ma <williamma@berkeley.edu>
2018-02-22 23:26:47 -0800

    Updated readme

--- generate_README.py
+++ generate_README.py
@@ -11,37 +11,7 @@ TODO:
 import os
 
 DEFAULT_README1 = '''
-# Project Title
-
-This project scraps the data of coins of Augustus (Octavian) from the years 44BC to 14 AD from the British Museum website. In doing so, we can explore any relationships of the inscriptions, denomination, material, date, subjects, associated names and weight.
-
-## Getting Started
-
-Simply run the code in Analyze Data.ipynb to see the relationships
-
-## Built With
-
-* [Pandas](https://pandas.pydata.org) - Handling the "large" amounts of data
-* And more...
-
-## Authors
-
-* **William Ma** - *Initial work* - [williamma12](https://github.com/williamma12)
-* **Professor Diliana Angelova** - *Historical Expert* - [University of California, Berkeley History of Art faculty page](http://arthistory.berkeley.edu/person/1637809-diliana-angelova)
-
-## License
-
-This project is licensed under the GLP-3.0 License - see the [LICENSE.md](LICENSE.md) file for details
-
-TODO:
-
- * Add Acknowledgements
- * Complete Built With list
- * Put location counts on map
-     * Include interactivity with simulated time passage
- * Explore other relationships
-
-## Plots:
+# Plots:
 
 '''
 
