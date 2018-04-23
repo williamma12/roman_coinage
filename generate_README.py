af3d04f848e195660f3716b2fe61b40f2e84bc13
William Ma <williamma@berkeley.edu>
2018-04-23 16:43:20 -0700

    Started updating readme to be more accessible

--- generate_README.py
+++ generate_README.py
@@ -2,10 +2,8 @@
 generate_README.py
 ~~~~~~~~~~~~~~~~~
 
-This file automatically generates the README.md to include links to all the plots.
+This file automatically generates a basic README.md to include links to all the plots.
 
-TODO:
-    * make readme better
 """
 
 import os
@@ -21,7 +19,7 @@ plot_files_str = "\n".join(plot_files)
 
 readme = DEFAULT_README1 + plot_files_str
 
-f = open('README.md', 'w')
+f = open('basic_README.md', 'w')
 f.write(readme)
 f.close()
 
